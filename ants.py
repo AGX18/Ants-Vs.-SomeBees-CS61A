2526508c00f49c29271001486b6d9dbac11cbf45
Omnia Fathy <agxnh2020@gmail.com>
2024-02-21 12:13:19 +0200

    P8b is done!!

--- ants.py
+++ ants.py
@@ -137,9 +137,16 @@ class Ant(Insect):
             place.ant = self
         else:
             # BEGIN Problem 8b
-            assert place.ant is None, 'Two ants in {0}'.format(place)
+            if place.ant.can_contain(self):  # the ant in the place is a container ant
+                place.ant.store_ant(self)
+            elif self.can_contain(place.ant):  # the ant added is a container ant
+                self.store_ant(place.ant)
+                Insect.add_to(self, place)
+                place.ant = self
+            else:
+                assert place.ant is None, 'Two ants in {0}'.format(place)
+
             # END Problem 8b
-        Insect.add_to(self, place)
 
     def remove_from(self, place):
         if place.ant is self:
